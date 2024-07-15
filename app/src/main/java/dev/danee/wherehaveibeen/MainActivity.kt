ab0bab922612eb78eef8dfcfd3a6e08fe1eb5677
daneeskripter <imdanee@seznam.cz>
2024-07-15 21:31:44 +0200

    navbar added

--- app/src/main/java/dev/danee/wherehaveibeen/MainActivity.kt
+++ app/src/main/java/dev/danee/wherehaveibeen/MainActivity.kt
@@ -2,10 +2,39 @@ package dev.danee.wherehaveibeen
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navBar = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        navBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.overview_button -> {
+                    replaceFragment(OverviewFragment())
+                    true
+                }
+                R.id.upload_button -> {
+                    replaceFragment(UploadFragment())
+                    true
+                }
+                R.id.account_button -> {
+                    replaceFragment(AccountFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentMgr = supportFragmentManager
+        val fragmentTransaction = fragmentMgr.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_container, fragment)
+        fragmentTransaction.commit()
     }
 }
